889f45b18ab7443f32674fe72e2e4f0bb4088276
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2023-02-21 14:24:24 +0000

    upgraded functional package version to 0.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,32 @@
 import { ObjectSerializer } from './core/serializer';
 export {
-  SerializerInterface,
-  SerializableBuilderInterface,
-  BuilderInterface,
+  BuilderInterface, SerializableBuilderInterface, SerializerInterface
 } from './contracts';
-
 export { JsonAttributes } from './internals';
 
-export const BuildObject = <T extends object = any>(
+export function BuildObject<T extends object = any>(
   blueprint: new () => T,
   from: Partial<T>
-) => {
+) {
   return new ObjectSerializer(blueprint).build(from);
-};
+}
 
-export const RebuildObject = <T extends object>(
-  instance: T,
-  from: Partial<T>
-) => {
+export function RebuildObject<T extends object>(instance: T, from: Partial<T>) {
   return new ObjectSerializer(instance.constructor as new () => T).rebuild(
     instance,
     from
   );
-};
+}
 
-export const SerializeObject = <T extends object>(instance: T) => {
+export function SerializeObject<T extends object>(instance: T) {
   return new ObjectSerializer(instance.constructor as new () => T).toSerialized(
     instance
   );
-};
+}
 
-export const DeserializeObject = <T extends object>(
+export function DeserializeObject<T extends object>(
   blueprint: new () => T,
   serialized: { [index: string]: any }
-) => {
+) {
   return new ObjectSerializer(blueprint).fromSerialized(serialized);
-};
+}
